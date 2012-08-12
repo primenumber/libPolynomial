43273e23d834ef4f92fc71b47aa88611a2fd17ad
prime number <primenumber.proj@gmail.com>
2012-08-13 00:20:09 +0900

    about license

--- polynomial.h
+++ polynomial.h
@@ -1,3 +1,25 @@
+/*
+ * polynomial.h
+ * This file is part of libPolynomial
+ *
+ * Copyright (C) 2012 - prime number
+ *
+ * libPolynomial is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libPolynomial is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libPolynomial. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+gplgplgplgpl
+
 #ifndef __POLY_H__
 #define __POLY_H__
 #include <string>
